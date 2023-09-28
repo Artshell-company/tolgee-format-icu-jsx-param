0e8bb3c38c66475a9c09bde0f93cb3d33716ded3
Matteo Dellea <matteo.dellea@artshell.eu>
2023-09-28 17:45:50 +0200

    build: setup project

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'build',
+      'chore',
+      'docs',
+      'feat',
+      'fix',
+      'perf',
+      'refactor',
+      'revert',
+      'style',
+      'test',
+      'release',
+      'translations',
+    ]],
+  },
+};
